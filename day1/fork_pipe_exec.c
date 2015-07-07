1df4c1f49a545486137515d66c312fe5e5e0e857
Vlad Tunnikov <doondook1@gmail.com>
2015-07-07 12:22:59 +0300

    ls -l work without sort

---
+++ day1/fork_pipe_exec.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+
+struct out {
+	char data;
+	char name;
+};
+
+int main() {
+  int fd[2];
+  pid_t pid;
+  char foo[4096];
+  char buf[1];
+  char tmp[256];
+	char delim[] = "";
+	char *ptr;
+	int i;
+	
+  if (pipe(fd)==-1){
+  	printf("ERROR: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+  if ((pid = fork()) == -1){
+			printf("ERROR: %s\n", strerror(errno));
+			exit(EXIT_FAILURE);
+	}
+  if(pid == 0) {
+
+    dup2 (fd[1], STDOUT_FILENO);
+    close(fd[0]);
+    close(fd[1]);
+    execl("/bin/ls", "ls", "-l", "/tmp", (char *)0);
+		printf("ERROR: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+
+  } else {
+
+	close(fd[1]);
+	//int nbytes = 
+	i = 0;
+	while (read(fd[0], &foo[i], 1)){
+		//foo[i] = &buf;
+		//printf("%s", &foo[i]);
+		if(buf[i] == '\n' || buf[i] == '\0'){
+			//printf("!");
+			strcpy(tmp, foo);
+			printf("%s", &tmp);
+			//printf("%s", &foo);
+			//ptr = strrchr(tmp,' ');
+			//if (ptr && *(ptr + 1))
+   			//printf("%s", ptr);
+			memset(foo, 0, sizeof(foo));
+			memset(tmp, 0, sizeof(tmp));
+			i = 0;
+			continue;
+		}
+		i++;
+	}
+	//printf("Output: (%.*s)\n", nbytes, foo);
+		wait(NULL);
+		exit(EXIT_SUCCESS);
+  }
+  
+
+  
+  return 0;
+}
