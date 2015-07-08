80a2549f17e55839d4257e21c2dd05a62eb47b24
Vlad Tunnikov <doondook1@gmail.com>
2015-07-08 22:51:38 +0300

    1

---
+++ day2/check_pass.c
@@ -0,0 +1,47 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+char password[] = "password";
+char str[30];
+
+pthread_cond_t wake_up;
+
+void *enter_pass()
+{
+	printf("Enter password:\n");
+	scanf("%s", str);
+	//pthread_cond_signal(&wake_up);
+	pthread_exit(NULL);
+}
+/*
+void *check_pass()
+{*/
+	
+
+int main(void)
+{
+	pthread_t thread1;
+	int rc;
+
+	rc = pthread_create(&thread1, NULL, enter_pass, NULL);
+	if (rc) {
+		printf("ERROR: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	while(1)
+	{
+		pthread_join(thread1, NULL);
+		if(!strcmp(str , password)) {
+			printf("true\n");
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			printf("Wrong password, try another\n");
+			rc = pthread_create(&thread1, NULL, enter_pass, NULL);
+		}
+	}
+}
